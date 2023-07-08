2863cabd93149361245923961db1b01d5201845a
NeoFlare <138781893+NeovimFlare@users.noreply.github.com>
2023-07-08 22:23:54 +0600

    Update dashboard.lua

--- lua/alpha/themes/dashboard.lua
+++ lua/alpha/themes/dashboard.lua
@@ -81,6 +81,7 @@ local buttons = {
         button("SPC f g", "󰈬  Find word"),
         button("SPC f m", "  Jump to bookmarks"),
         button("SPC s l", "  Open last session"),
+        button("l", "󰒲 LazyPM", "<cmd>Lazy<cmd>"),
     },
     opts = {
         spacing = 1,
